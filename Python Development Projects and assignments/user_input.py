b8172cde2bc3f66b2f4c5d4b0097bfb0c300af1e
Timothy Williams <timothywilliams.2410@gmail.com>
2024-03-11 19:38:22 +0300

    Added confirmation when the program terminates

--- Python Development Projects and assignments/user_input.py
+++ Python Development Projects and assignments/user_input.py
@@ -2,4 +2,4 @@ name = input("What is your name? ")
 age = input("What is your age? ")
 location = input("What is your location? ")
 print ("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
-
+print("End of the program")
