3f16dc29bd659fb159acf6808b723eef2b50b6c9
Timothy Williams <timothywilliams.2410@gmail.com>
2024-03-13 10:54:50 +0300

    thank you

--- Python Development Projects and assignments/user_input.py
+++ Python Development Projects and assignments/user_input.py
@@ -4,3 +4,4 @@ location = input("What is your location? ")
 print ("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
 print("End of the program")
 print("Thank you for using the program")
+
