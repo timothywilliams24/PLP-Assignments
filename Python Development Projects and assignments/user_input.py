f893f3cedfb763720e96abe43001123373d223d5
Timothy Williams <timothywilliams.2410@gmail.com>
2024-03-13 12:11:39 +0300

    n

--- Python Development Projects and assignments/user_input.py
+++ Python Development Projects and assignments/user_input.py
@@ -3,8 +3,4 @@ age = input("What is your age? ")
 location = input("What is your location? ")
 print ("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
 print("End of the program")
-<<<<<<<<< Temporary merge branch 1
-=========
 print("Thank you for using the program")
->>>>>>>>> Temporary merge branch 2
-
